3864f3b24e6d37a4948b37a515896cb561357ca8
abhishek kamat <abhishekkamat142@gmail.com>
2026-01-07 22:50:37 +0530

    Add autobind decorator and update ProjectInput class structure

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,22 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 //Object.defineProperty(exports, "__esModule", { value: true });
+function autobind(target, methodName, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        },
+    };
+    return adjDescriptor;
+}
 class ProjectInput {
     templateInput;
     hostElement;
@@ -8,14 +25,14 @@ class ProjectInput {
     descriptionInputElement;
     peopleInputElement;
     constructor() {
-        this.templateInput = document.getElementById('project-input');
-        this.hostElement = document.getElementById('app');
+        this.templateInput = document.getElementById("project-input");
+        this.hostElement = document.getElementById("app");
         const importedNode = document.importNode(this.templateInput.content, true);
         this.formElement = importedNode.firstElementChild;
-        this.formElement.id = 'user-input';
-        this.titleInputElement = this.formElement.querySelector('#title');
-        this.descriptionInputElement = this.formElement.querySelector('#description');
-        this.peopleInputElement = this.formElement.querySelector('#people');
+        this.formElement.id = "user-input";
+        this.titleInputElement = this.formElement.querySelector("#title");
+        this.descriptionInputElement = this.formElement.querySelector("#description");
+        this.peopleInputElement = this.formElement.querySelector("#people");
         this.configure();
         this.attach();
     }
@@ -24,10 +41,13 @@ class ProjectInput {
         console.log(this.titleInputElement.value);
     }
     configure() {
-        this.formElement.addEventListener('submit', this.submitHandler.bind(this));
+        this.formElement.addEventListener("submit", this.submitHandler);
     }
     attach() {
-        this.hostElement.insertAdjacentElement('afterbegin', this.formElement);
+        this.hostElement.insertAdjacentElement("afterbegin", this.formElement);
     }
 }
+__decorate([
+    autobind
+], ProjectInput.prototype, "submitHandler", null);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,61 @@
+function autobind(
+  target: any,
+  methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
 class ProjectInput {
-    templateInput: HTMLTemplateElement;
-    hostElement: HTMLDivElement;
-    formElement: HTMLFormElement;
-    titleInputElement: HTMLInputElement;
-    descriptionInputElement: HTMLInputElement;
-    peopleInputElement: HTMLInputElement;
+  templateInput: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  formElement: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
+  constructor() {
+    this.templateInput = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
 
-    constructor(){
-        this.templateInput = document.getElementById('project-input')! as HTMLTemplateElement;
-        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+    const importedNode = document.importNode(this.templateInput.content, true);
+    this.formElement = importedNode.firstElementChild as HTMLFormElement;
+    this.formElement.id = "user-input";
 
-        const importedNode = document.importNode(this.templateInput.content, true);
-        this.formElement = importedNode.firstElementChild as HTMLFormElement;
-        this.formElement.id = 'user-input';
+    this.titleInputElement = this.formElement.querySelector(
+      "#title"
+    )! as HTMLInputElement;
+    this.descriptionInputElement = this.formElement.querySelector(
+      "#description"
+    )! as HTMLInputElement;
+    this.peopleInputElement = this.formElement.querySelector(
+      "#people"
+    )! as HTMLInputElement;
 
-        this.titleInputElement = this.formElement.querySelector('#title')! as HTMLInputElement;
-        this.descriptionInputElement = this.formElement.querySelector('#description')! as HTMLInputElement;
-        this.peopleInputElement = this.formElement.querySelector('#people')! as HTMLInputElement;
+    this.configure();
+    this.attach();
+  }
 
-        this.configure()
-        this.attach();
-    }
+  @autobind
+  private submitHandler(e: Event) {
+    e.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
 
-    private submitHandler(e: Event) {
-        e.preventDefault();
-        console.log(this.titleInputElement.value);
-    }
+  private configure() {
+    this.formElement.addEventListener("submit", this.submitHandler);
+  }
 
-    private configure(){
-        this.formElement.addEventListener('submit', this.submitHandler.bind(this));
-    }
-
-    private attach() {
-        this.hostElement.insertAdjacentElement('afterbegin', this.formElement);
-    }
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.formElement);
+  }
 }
